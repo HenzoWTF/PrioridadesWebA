a0ac41c865284a8789a8eac2d8b7496bf408c7d4
HenzoWTF <Urenahenzo@gmail.com>
2024-03-02 13:31:22 -0400

    Context inyectado en el program

--- PrioridadesWebA.Api/Program.cs
+++ PrioridadesWebA.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PrioridadesWebA.Api.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options => options.UseSqlite("Data Source=PrioridadesWebA.db"));
 
 var app = builder.Build();
 
@@ -16,6 +21,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin(); // Permitir solicitudes desde cualquier origen
+    options.AllowAnyHeader(); // Permitir cualquier encabezado
+    options.AllowAnyMethod(); // Permitir cualquier método HTTP
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
