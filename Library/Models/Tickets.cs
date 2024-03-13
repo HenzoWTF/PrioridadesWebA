02ee58100a0d988701d474d3eb9e4e5d6deb23fe
HenzoWTF <Urenahenzo@gmail.com>
2024-03-13 13:47:01 -0400

    Ultimas modificaciones listas y funcionando completamente bien

--- Library/Models/Tickets.cs
+++ Library/Models/Tickets.cs
@@ -34,8 +34,4 @@ public class Tickets
 
     [Required(ErrorMessage = "El campo Descripcion se encuentra vacio")]
     public string? Descripcion { get; set; }
-
-
-	[ForeignKey("TicketId")]
-	public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
 }
